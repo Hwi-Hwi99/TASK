ae6e9ea66076ae7920421febf194bbfe537951d6
tuan129 <maithaituan129@gmail.com>
2025-05-20 12:28:32 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+// 1) MIDDLEWARES
+// console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+// 3) ROUTES
+
+module.exports = app;
